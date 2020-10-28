2917f05b6a03070cc3f403fc410a4f6ab3d57327
Michael Spinazze <mrspinaz@uwaterloo.ca>
2020-10-28 18:05:02 -0400

    Added variables

--- Numeral-Identifyer-Network/Network-Execution.py
+++ Numeral-Identifyer-Network/Network-Execution.py
@@ -1,6 +1,16 @@
+"""This code executes the neural network. The user can change the number of hidden layers, mini batch size,
+learning rate, and number of epochs to train the network over. The user can optionally include test data 
+to evaluate the network performace after each training epoch.
+"""
+
 import MNIST_Loader
 import Sigmoid_Network_Setup
 
+training_batch_size = 10
+learning_rate = 3.0
+number_of_epochs = 30
+
 training_data, validation_data, test_data = MNIST_Loader.load_data_wrapper()
-network = Sigmoid_Network_Setup.Network([784,30,10])
-network.stochastic_gradient_descent(training_data,10,3.0,30,test_data=test_data)
+network = Sigmoid_Network_Setup.Network([784, 30, 10])
+network.stochastic_gradient_descent(
+    training_data, training_batch_size, learning_rate, number_of_epochs, test_data=test_data)
