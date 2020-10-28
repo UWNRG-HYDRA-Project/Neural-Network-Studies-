feb7610e25ca5d3ca311b3177d054dc8eac3ec3a
Michael Spinazze <mrspinaz@uwaterloo.ca>
2020-10-28 18:06:44 -0400

    Added additional descriptions

--- Numeral-Identifyer-Network/Sigmoid_Network_Setup.py
+++ Numeral-Identifyer-Network/Sigmoid_Network_Setup.py
@@ -1,6 +1,8 @@
 """
 A module defining the network class containing the 
-stochastic gradient descent algorithm to optimize output correctness. Adapted from 
+stochastic gradient descent algorithm. Adapted from Michael Neilson's neural network 
+textbook example. Uses the MNIST handwritten digit database to train itself to identify handwritten
+numerals.
 
 """
 
@@ -18,9 +20,9 @@ class Network:
         """
         self.neuronLayers = len(neuronNums)
         self.neuronNums = neuronNums
-        self.biases = [np.random.randn(y,1) for y in neuronNums[1:]]
-        self.weights = [np.random.randn(y,x) for x,y in zip(neuronNums[:-1], neuronNums[1:])]
-
+        self.biases = [np.random.randn(y, 1) for y in neuronNums[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(
+            neuronNums[:-1], neuronNums[1:])]
 
     def feedforwardoutput(self, a):
         """Returns the output of the network given the input ``a`` where ``a`` is an array of 28 * 28 = 784
@@ -41,34 +43,19 @@ class Network:
             n_tests = len(test_data)
 
         n = len(training_data)
-        
+
         for j in range(epoches):
             random.shuffle(training_data)
-            mini_batches = [training_data[k:k + mini_batch_size] for k in range(0,n,mini_batch_size)]
+            mini_batches = [training_data[k:k + mini_batch_size]
+                            for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_weights_and_biases(mini_batch, eta)
             if test_data is not None:
-                print("Epoch {0}: {1} correct IDs / {2} samples".format(j,self.evaluate(test_data), n_tests))
+                print("Epoch {0}: {1} correct IDs / {2} samples".format(j,
+                                                                        self.evaluate(test_data), n_tests))
 
             else:
                 print("Epoch {0} completed".format(j))
-
-
-    # def update_mini_batch(self, mini_batch, eta):
-    #     """Update the network's weights and biases by applying
-    #     gradient descent using backpropagation to a single mini batch.
-    #     The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-    #     is the learning rate."""
-    #     nabla_b = [np.zeros(b.shape) for b in self.biases]
-    #     nabla_w = [np.zeros(w.shape) for w in self.weights]
-    #     for x, y in mini_batch:
-    #         delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-    #         nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-    #         nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-    #     self.weights = [w-(eta/len(mini_batch))*nw
-    #                     for w, nw in zip(self.weights, nabla_w)]
-    #     self.biases = [b-(eta/len(mini_batch))*nb
-    #                    for b, nb in zip(self.biases, nabla_b)]
 
     def update_weights_and_biases(self, mini_batch, eta):
         """Updates weights and biases of the network using the training examples from a single mini-batch. 
@@ -84,46 +71,49 @@ class Network:
             delta_del_b, delta_del_w = self.backpropogation(x, y)
             del_b = [single_layer_del_b + single_layer_delta_del_b
                      for single_layer_del_b, single_layer_delta_del_b in zip(del_b, delta_del_b)]
-            del_w = [single_layer_del_w + single_layer_delta_del_w for single_layer_del_w, single_layer_delta_del_w in zip(del_w, delta_del_w)]
+            del_w = [single_layer_del_w + single_layer_delta_del_w for single_layer_del_w,
+                     single_layer_delta_del_w in zip(del_w, delta_del_w)]
         self.weights = [w - (eta/len(mini_batch))*nw for w,
                         nw in zip(self.weights, del_w)]
         self.biases = [b - (eta/len(mini_batch))*db for b,
                        db in zip(self.biases, del_b)]
 
-
     def backpropogation(self, x, y):
-            """Returns a tuple representing the gradient of the cost function for a single training example. 
-            I.e. the partial derivatives of the cost function with respect to every weight and bias in the 
-            network. 
-            """
-            del_b = [np.zeros(b.shape) for b in self.biases]
-            del_w = [np.zeros(w.shape) for w in self.weights]
+        """Returns a tuple representing the gradient of the cost function for a single training example. 
+        I.e. the partial derivatives of the cost function with respect to every weight and bias in the 
+        network. 
+        """
+        del_b = [np.zeros(b.shape) for b in self.biases]
+        del_w = [np.zeros(w.shape) for w in self.weights]
 
-            activation = x
-            # Stores activation vectors layer by layer. Initialized with input activation x
-            activation_list = [x]
-            z_vectors = []
+        activation = x
+        # Stores activation vectors layer by layer. Initialized with input activation x
+        activation_list = [x]
+        z_vectors = []
 
-            # Feedforward. Calculates the activation vector for each layer
-            for b, w in zip(self.biases, self.weights):
-                z = np.dot(w, activation) + b
-                z_vectors.append(z)
-                activation = sigmoid(z)
-                activation_list.append(activation)
+        # Feedforward. Calculates the activation vector for each layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            z_vectors.append(z)
+            activation = sigmoid(z)
+            activation_list.append(activation)
 
-            # Calculates the error in the output layer
-            output_error = self.cost_prime(activation_list[-1], y)*sigmoid_derivative(z_vectors[-1])
+        # Calculates the error in the output layer
+        output_error = self.cost_prime(
+            activation_list[-1], y)*sigmoid_derivative(z_vectors[-1])
 
-            # Backpropogates the output error to calculate the error in every other layer of the network.
-            del_b[-1] = output_error
-            del_w[-1] = np.dot(output_error, activation_list[-2].transpose())
-            for i in range(2, self.neuronLayers):
-                z = z_vectors[-i]
-                sd = sigmoid_derivative(z)
-                output_error = np.dot(self.weights[-i + 1].transpose(), output_error) * sd
-                del_b[-i] = output_error
-                del_w[-i] = np.dot(output_error, activation_list[-i - 1].transpose())
-            return(del_b, del_w)
+        # Backpropogates the output error to calculate the error in every other layer of the network.
+        del_b[-1] = output_error
+        del_w[-1] = np.dot(output_error, activation_list[-2].transpose())
+        for i in range(2, self.neuronLayers):
+            z = z_vectors[-i]
+            sd = sigmoid_derivative(z)
+            output_error = np.dot(
+                self.weights[-i + 1].transpose(), output_error) * sd
+            del_b[-i] = output_error
+            del_w[-i] = np.dot(output_error,
+                               activation_list[-i - 1].transpose())
+        return(del_b, del_w)
 
     def cost_prime(self, output_activations, y):
         """The derivative of the cost function used in the backpropogation algorithm. Returns a vector of
@@ -135,8 +125,8 @@ class Network:
 
         test_results = [(np.argmax(self.feedforwardoutput(x)), y)
                         for (x, y) in test_data]
-    
-        return sum(int(x ==y ) for (x,y) in test_results)
+
+        return sum(int(x == y) for (x, y) in test_results)
 
 
 def sigmoid(z):
